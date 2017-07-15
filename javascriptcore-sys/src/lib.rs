1e9c02448bb1b0fdf09732f198f68c941c0cb899
Bruce Mitchener <bruce.mitchener@gmail.com>
2017-07-15 14:20:12 +0700

    Add explanatory comment for jsc-sys crate.

--- javascriptcore-sys/src/lib.rs
+++ javascriptcore-sys/src/lib.rs
@@ -4,6 +4,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! This crate provides raw bindings for the JavaScriptCore public
+//! API. It is a pretty direct mapping of the underlying C API
+//! provided by JavaScriptCore.
+
 #![allow(non_camel_case_types, non_snake_case)]
 
 mod javascriptcore_sys;
