2e79f4690fe7f3d56faf701c6764cedc94514ab7
Ivan Enderlin <ivan@mnt.io>
2023-10-12 10:58:32 +0200

    feat: `JSException` implements `Display` and `Error`.
    This patch implements `Display` and `Error` for `JSException`.

--- src/exception.rs
+++ src/exception.rs
@@ -4,6 +4,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::{error, fmt};
+
 use crate::{sys, JSException, JSString, JSValue};
 
 impl JSException {
@@ -15,13 +17,20 @@ impl JSException {
     /// Return the name of the exception. This is the value of the `name`
     /// property on the exception object.
     pub fn name(&self) -> Result<JSString, JSException> {
-        self.value
-            .as_object()
-            .unwrap()
-            .get_property("name")
-            .as_string()
+        self.value.as_object()?.get_property("name").as_string()
     }
 }
+
+impl fmt::Display for JSException {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.underlying_value().as_string() {
+            Ok(string) => write!(formatter, "JSException (interpreted as string): {}", string),
+            Err(_) => write!(formatter, "{:?}", self),
+        }
+    }
+}
+
+impl error::Error for JSException {}
 
 impl From<JSValue> for JSException {
     fn from(value: JSValue) -> Self {
