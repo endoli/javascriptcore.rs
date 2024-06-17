af7f438dc8aaf7d5b875d52f5c87459a899a8425
Ryan Daum <ryan.daum@gmail.com>
2024-06-17 15:13:24 -0400

    Check for javascriptcoregtk-4.1 in addition to 4.0 and 3.0

--- javascriptcore-sys/build.rs
+++ javascriptcore-sys/build.rs
@@ -4,18 +4,26 @@ fn main() {
 }
 
 #[cfg(target_os = "linux")]
+const POTENTIAL_LIBS: [&str; 3] = [
+    "javascriptcoregtk-4.1",
+    "javascriptcoregtk-4.0",
+    "javascriptcoregtk-3.0",
+];
+
+#[cfg(target_os = "linux")]
 fn main() {
     println!("cargo:rerun-if-env-changed=DOCS_RS");
     if std::env::var("DOCS_RS").is_ok() {
         return;
     }
-    let r = pkg_config::probe_library("javascriptcoregtk-4.0");
-    if r.is_err() {
-        let r = pkg_config::probe_library("javascriptcoregtk-3.0");
-        if r.is_err() {
-            panic!("libjavascriptcoregtk-3.0-dev or -4.0-dev must be installed.")
+
+    for l in POTENTIAL_LIBS {
+        let r = pkg_config::probe_library(l);
+        if r.is_ok() {
+            return;
         }
     }
+    panic!("libjavascriptcoregtk-4.0, 4.1 or 3.0 must be installed.");
 }
 
 #[cfg(not(any(target_os = "macos", target_os = "linux")))]
